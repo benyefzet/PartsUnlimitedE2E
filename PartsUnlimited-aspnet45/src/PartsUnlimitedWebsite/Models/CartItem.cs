981930322e1c962fedd5405178f46af3bee1ccf7
benyefzet <benyfz1337@gmail.com>
2026-02-14 10:12:21 +0100

    PTX-3023 PTX-3021:Change catogory name

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        // My first change to be commited.
+        // My third change.
         [Key]
         public int CartItemId { get; set; }
 
